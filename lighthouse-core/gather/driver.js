9499a3c9810e1307bcd97ffa6dc27574d4b344de
Paul Irish <paul.irish@gmail.com>
2017-06-02 18:51:12 -0700

    run a timeout against our handleJavaScriptDialog.

--- lighthouse-core/gather/driver.js
+++ lighthouse-core/gather/driver.js
@@ -920,17 +920,27 @@ class Driver {
   }
 
   /**
-   * Dismiss JavaScript dialogs (alert, confirm, prompt), providing a
-   * generic promptText in case the dialog is a prompt.
+   * Dismiss JavaScript dialogs (alert, confirm, prompt), but quit everything if we fail to dismiss
    * @return {!Promise}
    */
   dismissJavaScriptDialogs() {
+    const waitForDismiss = 300;
+    const silent = {silent: true};
+
+
     return this.sendCommand('Page.enable').then(_ => {
       // No need to wait for this event bind to complete, so no promise returned
       this.on('Page.javascriptDialogOpening', data => {
-        log.warn('Driver', `${data.type} dialog opened by the page automatically suppressed.`);
-        this.sendCommand('Page.handleJavaScriptDialog', {accept: false}, {silent: true})
-        .catch(_ => this.sendCommand('Page.handleJavaScriptDialog', {accept: true}, {silent: true}));
+        log.warn('Driver', `${data.type} dialog opened. Attempting to dismiss...`);
+
+        const waitTimeoutID = setTimeout(_ => {
+          Promise.reject(new Error(`handleJavaScriptDialog took over ${waitForDismiss}ms`));
+        }, waitForDismiss);
+
+        this.sendCommand('Page.handleJavaScriptDialog', {accept: false}, silent)
+          .catch(_ => this.sendCommand('Page.handleJavaScriptDialog', {accept: true}, silent))
+          .catch(e => log.warn('Driver', e))
+          .then(_ => clearTimeout(waitTimeoutID));
       });
     });
   }
