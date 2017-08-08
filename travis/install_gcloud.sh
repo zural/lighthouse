3136c69e0ed0160dc2b1e912666bbf26304ae2bb
Ward Peeters <ward@coding-tech.com>
2017-08-08 06:58:52 +0200

    Remove default env

---
+++ travis/install_gcloud.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Download & install the Google Cloud SDK
+if [ ! -d $HOME/google-cloud-sdk/bin ]; then
+  rm -rf $HOME/google-cloud-sdk;
+  curl https://sdk.cloud.google.com | bash;
+fi;
+
+source ${HOME}/google-cloud-sdk/path.bash.inc
