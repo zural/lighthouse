36a68484e32dd9c0a2bf1a8e6e77a144b116f557
Ward Peeters <ward@coding-tech.com>
2017-10-12 23:02:20 +0200

    Review changes

--- lighthouse-core/audits/page-execution-timings.js
+++ lighthouse-core/audits/page-execution-timings.js
@@ -14,72 +14,88 @@ const Audit = require('./audit');
 const Util = require('../report/v2/renderer/util');
 const DevtoolsTimelineModel = require('../lib/traces/devtools-timeline-model');
 
-const timelineCategories = [
-  {
-    name: 'Compile Script',
-    group: 'JavaScript',
-  },
-  {
-    name: 'Evaluate Script',
-    group: 'JavaScript',
-  },
-  {
-    name: 'Run Microtasks',
-    group: 'JavaScript',
-  },
-  {
-    name: 'Minor GC',
-    group: 'JavaScript',
-  },
-  {
-    name: 'Major GC',
-    group: 'JavaScript',
-  },
-  {
-    name: 'XHR Ready State Change',
-    group: 'JavaScript',
-  },
-  {
-    name: 'XHR Load',
-    group: 'JavaScript',
-  },
-  {
-    name: 'Layout',
-    group: 'Paint/Layout',
-  },
-  {
-    name: 'Paint',
-    group: 'Paint/Layout',
-  },
-  {
-    name: 'Composite Layers',
-    group: 'Paint/Layout',
-  },
-  {
-    name: 'Update Layer Tree',
-    group: 'Paint/Layout',
-  },
-  {
-    name: 'Recalculate Style',
-    group: 'Paint/Layout',
-  },
-  {
-    name: 'Parse HTML',
-    group: 'DOM/CSS',
-  },
-  {
-    name: 'Parse Stylesheet',
-    group: 'DOM/CSS',
-  },
-  {
-    name: 'DOM GC',
-    group: 'DOM/CSS',
-  },
-  {
-    name: 'Image Decode',
-    group: 'Images',
-  },
-];
+const group = {
+  loading: 'Network request loading',
+  parseHTML: 'Parsing DOM',
+  styleLayout: 'Style & Layout',
+  compositing: 'Compositing',
+  painting: 'Paint',
+  gpu: 'GPU',
+  scripting: 'Script Evaluation',
+  scriptParseCompile: 'Script Parsing & Compile',
+  scriptGC: 'Garbage collection',
+  other: 'Other',
+  images: 'Images',
+};
+
+const taskToGroup = {
+  'Animation': group.painting,
+  'Async Task': group.other,
+  'Frame Start': group.painting,
+  'Frame Start (main thread)': group.painting,
+  'Cancel Animation Frame': group.scripting,
+  'Cancel Idle Callback': group.scripting,
+  'Compile Script': group.scriptParseCompile,
+  'Composite Layers': group.compositing,
+  'Console Time': group.scripting,
+  'Image Decode': group.images,
+  'Draw Frame': group.painting,
+  'Embedder Callback': group.scripting,
+  'Evaluate Script': group.scripting,
+  'Event': group.scripting,
+  'Animation Frame Fired': group.scripting,
+  'Fire Idle Callback': group.scripting,
+  'Function Call': group.scripting,
+  'DOM GC': group.scriptGC,
+  'GC Event': group.scriptGC,
+  'GPU': group.gpu,
+  'Hit Test': group.compositing,
+  'Invalidate Layout': group.styleLayout,
+  'JS Frame': group.scripting,
+  'Input Latency': group.scripting,
+  'Layout': group.styleLayout,
+  'Major GC': group.scriptGC,
+  'DOMContentLoaded event': group.scripting,
+  'First paint': group.painting,
+  'FMP': group.painting,
+  'FMP candidate': group.painting,
+  'Load event': group.scripting,
+  'Minor GC': group.scriptGC,
+  'Paint': group.painting,
+  'Paint Image': group.images,
+  'Paint Setup': group.painting,
+  'Parse Stylesheet': group.parseHTML,
+  'Parse HTML': group.parseHTML,
+  'Parse Script': group.scriptParseCompile,
+  'Other': group.other,
+  'Rasterize Paint': group.painting,
+  'Recalculate Style': group.styleLayout,
+  'Request Animation Frame': group.scripting,
+  'Request Idle Callback': group.scripting,
+  'Request Main Thread Frame': group.painting,
+  'Image Resize': group.images,
+  'Finish Loading': group.loading,
+  'Receive Data': group.loading,
+  'Receive Response': group.loading,
+  'Send Request': group.loading,
+  'Run Microtasks': group.scripting,
+  'Schedule Style Recalculation': group.styleLayout,
+  'Scroll': group.compositing,
+  'Task': group.other,
+  'Timer Fired': group.scripting,
+  'Install Timer': group.scripting,
+  'Remove Timer': group.scripting,
+  'Timestamp': group.scripting,
+  'Update Layer': group.compositing,
+  'Update Layer Tree': group.compositing,
+  'User Timing': group.scripting,
+  'Create WebSocket': group.scripting,
+  'Destroy WebSocket': group.scripting,
+  'Receive WebSocket Handshake': group.scripting,
+  'Send WebSocket Handshake': group.scripting,
+  'XHR Load': group.scripting,
+  'XHR Ready State Change': group.scripting,
+};
 
 class PageExecutionTimings extends Audit {
 
@@ -100,24 +116,15 @@ class PageExecutionTimings extends Audit {
 
   /**
    * @param {!Array<TraceEvent>=} trace
-   * @param {!WebInspector.TimelineProfileTree.Node} A grouped and sorted tree
-   */
-  static getTimingsByCategory(trace) {
-    const timelineModel = new DevtoolsTimelineModel(trace);
-
-    return timelineModel.bottomUpGroupBy('EventName');
-  }
-
-  /**
-   * @param {!Array<TraceEvent>=} trace
    * @return {!Map<string, Number>}
    */
   static getExecutionTimingsByCategory(trace) {
-    const bottomUpByName = PageExecutionTimings.getTimingsByCategory(trace);
+    const timelineModel = new DevtoolsTimelineModel(trace);
+    const bottomUpByName = timelineModel.bottomUpGroupBy('EventName');
 
     const result = new Map();
     bottomUpByName.children.forEach((value, key) =>
-      result.set(key, value.selfTime.toFixed(1)));
+      result.set(key, Number(value.selfTime.toFixed(1))));
 
     return result;
   }
@@ -131,15 +138,16 @@ class PageExecutionTimings extends Audit {
     const executionTimings = PageExecutionTimings.getExecutionTimingsByCategory(trace);
     let totalExecutionTime = 0;
 
-    const results = timelineCategories.map(category => {
-      const timing = Number(executionTimings.get(category.name) || 0);
-      totalExecutionTime += timing;
+    const extendedInfo = {};
+    const results = Array.from(executionTimings).map(([eventName, duration]) => {
+      totalExecutionTime += duration;
+      extendedInfo[eventName] = duration;
 
       return {
-        category: category.name,
-        group: category.group,
-        duration: Util.formatMilliseconds(timing, 1),
-      }
+        category: eventName,
+        group: taskToGroup[eventName],
+        duration: Util.formatMilliseconds(duration, 1),
+      };
     });
 
     const headings = [
@@ -154,6 +162,9 @@ class PageExecutionTimings extends Audit {
       rawValue: totalExecutionTime,
       displayValue: Util.formatMilliseconds(totalExecutionTime),
       details: tableDetails,
+      extendedInfo: {
+        value: extendedInfo,
+      },
     };
   }
 }
