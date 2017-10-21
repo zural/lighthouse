75b039fc995eedf2de322d56cb9bd9dd144a4b37
Ward Peeters <ward@coding-tech.com>
2017-10-22 00:59:17 +0200

    fix formatting for changelog

---
+++ conventional-changelog-lighthouse/index.js
@@ -0,0 +1,50 @@
+'use strict';
+const readFileSync = require('fs').readFileSync;
+const resolve = require('path').resolve;
+
+const parserOpts = {
+  headerPattern: /^(\w*)(?:\((.*)\))?\: (.*)$/,
+  headerCorrespondence: [
+    'type',
+    'scope',
+    'message',
+  ],
+};
+
+const writerOpts = {
+  transform: commit => {
+    if (typeof commit.hash === 'string') {
+      commit.hash = commit.hash.substring(0, 7);
+    }
+
+    if (commit.type) {
+      commit.type = commit.type.replace(/_/g, ' ');
+      commit.type = commit.type.substring(0, 1).toUpperCase() + commit.type.substring(1);
+    } else {
+      commit.type = 'Misc';
+    }
+
+    return commit;
+  },
+  groupBy: 'type',
+  commitGroupsSort: (a, b) => {
+    if (b.title === 'New audit') {
+      return 1;
+    }
+
+    return a.title.localeCompare(b.title);
+  },
+  commitsSort: ['type', 'scope', 'message']
+};
+
+const template = readFileSync(resolve(__dirname, 'templates/template.hbs')).toString();
+const header = readFileSync(resolve(__dirname, 'templates/header.hbs')).toString();
+const commit = readFileSync(resolve(__dirname, 'templates/commit.hbs')).toString();
+writerOpts.mainTemplate = template;
+writerOpts.headerPartial = header;
+writerOpts.commitPartial = commit;
+
+module.exports = {
+  writerOpts,
+  parserOpts,
+};
